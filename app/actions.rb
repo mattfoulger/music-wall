1c38cfcd1d5221281a9004e3617b09e056422b34
mattfoulger <mfoulger@gmail.com>
2015-07-14 12:33:19 -0700

    add new track

--- app/actions.rb
+++ app/actions.rb
@@ -7,3 +7,21 @@ get '/tracks' do
   @tracks = Track.all
   erb :'tracks/index'
 end
+
+get '/tracks/new' do
+  @track = Track.new
+  erb :'tracks/new'
+end
+
+post '/tracks' do
+  @track = Track.new(
+    song_title:   params[:song_title],
+    author: params[:author],
+    url:  params[:url]
+  )
+  if @track.save
+    redirect '/tracks'
+  else
+    erb :'tracks/new'
+  end
+end
